50958e147faed3445e14bf9c54875067f572cc5d
Peter Stuart <peter@phpete.com>
2025-02-01 11:25:07 +0000

    add about content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about flex flex-col space-y-4 text-left">
+    <h1 class="text-4xl">quickflix (phase 1)</h1>
+    <div>
+      <p>
+        quickflix is an educational project by phpete. It is my first Vue app
+        I've made.
+      </p>
+      <p>
+        My objectives in this app was to put various aspects of Vue.js into
+        practise.
+      </p>
+    </div>
+    <div>
+      <p>Phase 1: which is to have a working app without a backend.</p>
+      <p>Phase 2: implement a nodejs backend.</p>
+      <p>Phase 3: implement a Inertia backend with Laravel.</p>
+    </div>
   </div>
 </template>
